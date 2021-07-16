79280ef370c8e190d604cd024751fca23ad0fa26
tabrasel <tatebrasel@gmail.com>
2021-07-16 01:36:35 -0700

    style: use prettier type names

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express, { Application, Request, Response } from 'express';
+import { Connection, Schema, connect, connection, model } from 'mongoose';
 
-const app: express.Application = express();
+const app: Application = express();
 const port: number = 80;
 
 const DB_HOST_URL: string = 'mongodb+srv://tatebrasel:7hQfLPox7nZ6jB9T@cluster1.m1uha.mongodb.net/music-share?retryWrites=true&w=majority';
-mongoose.connect(DB_HOST_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+connect(DB_HOST_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db: mongoose.Connection = mongoose.connection;
+const db: Connection = connection;
 
 db.once('open', () => {
   // tslint:disable-next-line:no-console
@@ -19,7 +19,7 @@ db.on('error', () => {
   console.error.bind(console, 'connection error:')
 });
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!');
 })
 
