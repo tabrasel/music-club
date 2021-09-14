bfd7d8e5ee1257676217afed36581cd2fd686110
tabrasel <tatebrasel@gmail.com>
2021-09-13 23:40:44 -0700

    feat: add script for populating rounds with their participants

---
+++ src/setRoundParticipantIds.ts
@@ -0,0 +1,39 @@
+/*
+  Sets each round's particpantIds by using album poster IDs.
+*/
+
+import { Database } from './Database';
+
+// Import models
+import { AlbumModel } from './models/AlbumModel';
+import { RoundModel } from './models/RoundModel';
+
+// Connect to database
+Database.connect();
+
+// Set up models
+AlbumModel.setup();
+RoundModel.setup();
+
+// Get all rounds
+RoundModel.model.find({}, (err: any, allRounds: any) => {
+
+  for (const round of allRounds) {
+    if (round.participantIds.length !== 0) continue;
+
+    // Get promises to load each round
+    const albumPromises: any[] = round.albumIds.map((albumId: string) => {
+      return AlbumModel.model.findOne({ id: albumId }, (err2: any, album: any) => {
+        Promise.resolve(album);
+      });
+    });
+
+    // Set particpantIds once all rounds are loaded
+    Promise.all(albumPromises).then((albums: any[]) => {
+      const filter: any = { id: round.id };
+      const updatedData: any = { participantIds: albums.map((album: any) => album.posterId) };
+      RoundModel.model.findOneAndUpdate(filter, updatedData, { new: true, useFindAndModify: false, strict: false }).exec();
+    });
+  }
+
+});
