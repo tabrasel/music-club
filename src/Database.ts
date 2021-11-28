49a95775b2745adcb3cfc45674013ad03cf2536e
tabrasel <tatebrasel@gmail.com>
2021-11-28 13:25:16 -0800

    feat: add database disconnect function

--- src/Database.ts
+++ src/Database.ts
@@ -1,21 +1,26 @@
-import { Connection, connect, connection } from 'mongoose';
+// import { Connection, connect, connection } from 'mongoose';
+import mongoose from 'mongoose';
 
 class Database {
 
   private static DATABASE_URI: string = 'mongodb+srv://tatebrasel:7hQfLPox7nZ6jB9T@cluster1.m1uha.mongodb.net/music-share?retryWrites=true&w=majority';
 
   static connect(): void {
-    connection.once('open', () => {
+    mongoose.connection.once('open', () => {
       // tslint:disable-next-line:no-console
       console.log('Connected to database');
     });
 
-    connection.on('error', () => {
+    mongoose.connection.on('error', () => {
       // tslint:disable-next-line:no-console
       console.error.bind(console, 'connection error:')
     });
 
-    connect(this.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(this.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+  }
+
+  static disconnect(): void {
+    mongoose.disconnect();
   }
 
 }
