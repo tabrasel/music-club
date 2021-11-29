0cf049548646fd0353fdcd3ff1ba0444a1c62487
tabrasel <tatebrasel@gmail.com>
2021-11-28 23:12:18 -0800

    chore: update mongoose to v6.0.13

--- src/Database.ts
+++ src/Database.ts
@@ -16,7 +16,7 @@ class Database {
       console.error.bind(console, 'connection error:')
     });
 
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(process.env.MONGO_URI);
   }
 
   static disconnect(): void {
