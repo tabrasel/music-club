99003db47d338affe8602e3cc7b9231625de40f6
tabrasel <tatebrasel@gmail.com>
2022-01-13 23:54:02 -0800

    refactor: harmonize spotify request retries

--- src/routes/spotify_routes.ts
+++ src/routes/spotify_routes.ts
@@ -5,7 +5,7 @@ import store from 'store2';
 
 const router: Router = Router();
 
-function getAccessToken(): void {
+function updateAccessToken(): void {
   axios({
     url: 'https://accounts.spotify.com/api/token',
     method: 'post',
@@ -67,58 +67,67 @@ function fetchArtist(id: string) {
 
 // Search for an album
 router.get('/api/album-search', async (req: any, res: Response) => {
+  // Try making the request
   try {
     const searchResult: any = await searchForAlbum(req.query.q);
     res.json(searchResult.data.albums);
   } catch(err) {
+    // Error if the issue wasn't authorization
     if (err.response.status !== 401) {
       res.status(err.response.status);
       res.send(err.response.statusText);
       return;
     }
 
-    // If the initial request was unauthorized, request a new token and try again
-    axios({
-      url: 'https://accounts.spotify.com/api/token',
-      method: 'post',
-      params: {
-        grant_type: 'client_credentials'
-      },
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      auth: {
-        username: process.env.SPOTIFY_CLIENT_ID,
-        password: process.env.SPOTIFY_CLIENT_SECRET
-      }
-    })
-    .then(async (tokenRes) => {
-      store.set('spotifyAccessToken', tokenRes.data.access_token);
-      try {
-        const searchResultRetry: any = await searchForAlbum(req.query.q);
-        res.json(searchResultRetry.data.albums);
-      } catch(searchResultRetryErr) {
-        res.status(searchResultRetryErr.response.status);
-        res.send(searchResultRetryErr.response.statusText);
-      }
-    })
-    .catch((tokenErr) => {
+    // Try updating the access token
+    try {
+      await updateAccessToken();
+    } catch(tokenErr) {
       res.status(tokenErr.response.status);
       res.send(tokenErr.response.statusText);
-    });
+      return;
+    }
+
+    // Retry the request
+    try {
+      const searchResult: any = await searchForAlbum(req.query.q);
+      res.json(searchResult.data.albums);
+    } catch(retryErr) {
+      res.status(err.response.status);
+      res.send(err.response.statusText);
+    }
   }
 });
 
 router.get('/api/artist', async (req: any, res: Response) => {
+  // Try making the request
   try {
     const artistResult: any = await fetchArtist(req.query.id);
     res.json(artistResult.data);
   } catch(err) {
+    // Error if the issue wasn't authorization
     if (err.response.status !== 401) {
       res.status(err.response.status);
       res.send(err.response.statusText);
       return;
+    }
+
+    // Try updating the access token
+    try {
+      await updateAccessToken();
+    } catch(tokenErr) {
+      res.status(tokenErr.response.status);
+      res.send(tokenErr.response.statusText);
+      return;
+    }
+
+    // Retry the request
+    try {
+      const artistResult: any = await fetchArtist(req.query.id);
+      res.json(artistResult.data);
+    } catch(retryErr) {
+      res.status(err.response.status);
+      res.send(err.response.statusText);
     }
   }
 });
